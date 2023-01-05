5da34b3c499fda1949d6b3cf53898a076ff9315b
Jerry Chen <Jerrychen.works@gmail.com>
2023-01-05 10:32:26 +0800

    Add files via upload

---
+++ monitor_main.py
@@ -0,0 +1,10 @@
+import smart_value
+
+
+def update_monitor():
+    """Update the pipeline monitor"""
+
+    o = smart_value.tools.monitor.Monitor()
+
+if __name__ == '__main__':
+    update_monitor()
